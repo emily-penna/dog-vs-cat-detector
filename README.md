4f46805cbb68664559855422f70336c0a77101d4
Emily P <emilypenna99@gmail.com>
2024-01-12 12:19:11 -0500

    trained on 100 epochs

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # dog vs cat detector
 dog and cat image dataset used to train model from kaggle: https://www.kaggle.com/datasets/samuelcortinhas/cats-and-dogs-image-classification/data
+
--- detect.py
+++ detect.py
@@ -1,10 +1,7 @@
 from ultralytics import YOLO
 
-# Load modelt
+# Load model
 model = YOLO('best.pt')
 
 # Use the model
-# results = model.train(data='coco128.yaml', epochs=3)  # train the model
-# results = model.val()  # evaluate model performance on the validation set
-model.predict(source='images', save=True)  # predict on an image
-# results = model.export(format='onnx')  # export the model to ONNX format
+model.predict(source='images', save=True)
