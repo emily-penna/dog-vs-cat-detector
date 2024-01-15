e30172d42ede92553728f5a1a06f42591d6ffdba
Emily P <emilypenna99@gmail.com>
2024-01-15 17:12:20 -0500

    access box data from analyzed images

--- detect.py
+++ detect.py
@@ -1,7 +1,20 @@
 from ultralytics import YOLO
+import cv2
+import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
 
 # Load model
 model = YOLO('best.pt')
-
 # Use the model
-model.predict(source='images', save=True)
+# model.predict(source='cat.jpg')
+results = model('images', save=True, conf=0.5)
+for result in results:
+    boxes = result.boxes  # Boxes object for bbox outputs
+    print(boxes)
+
+# image_input = cv2.imread('cat.jpg')
+
+# plt.rcParams['figure.figsize'] = (10.0, 10.0)
+# plt.imshow(cv2.cvtColor(image_input, cv2.COLOR_BGR2RGB))
+# plt.show()
